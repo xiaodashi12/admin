6c14611eef7fbda52d03a0ede3a327f6e3b88c37
ChenJG <1207688613@qq.com>
2019-04-17 23:19:36 +0800

    更换图标，修改配置

--- src/store.js
+++ src/store.js
@@ -3,20 +3,43 @@ import Vuex from 'vuex'
 import { getToken, setToken, removeToken, getLocalStorage, setLocalStorage,clearLocalStorage } from '@/utils/auth'
 import fetch from '@/utils/fetch'
 import api from '@/api'
-
+import storage from '@/utils/storage.js'
+import axios from 'axios'
 Vue.use(Vuex)
-
+Vue.use(axios)
 export default new Vuex.Store({
   state: {
     ticket:getToken(),
     operatorInfo:getLocalStorage('operatorInfo') || {},//系统操作员
     customerInfo:{},//ETC客户
-    vehicleInfo:{}//车辆信息
+    vehicleInfo:{},//车辆信息
+    musicList: {'musicData': []},
+    theme: 'red',
+    isPlaying: false,
+    nowPlayIndex: 0,
+    showMiniAudio: true,
+    commendList: {'commendList': []},
+    curTimeNum: 0,
+    showPlay: false,
+    showAbout: false,
+    showLogin: false,
+    day: {'musicData': []},
   },
   getters:{
     operatorInfo: (state) => state.operatorInfo,
     customerInfo: (state) => state.customerInfo,
-    vehicleInfo:(state) => state.vehicleInfo
+    vehicleInfo:(state) => state.vehicleInfo,
+    musicList:(state) => state.musicList,
+    theme:(state) => state.theme,
+    isPlaying:(state) => state.isPlaying,
+    nowPlayIndex:(state) => state.nowPlayIndex,
+    showMiniAudio:(state) => state.showMiniAudio,
+    commendList:(state) => state.commendList,
+    curTimeNum:(state) => state.curTimeNum,
+    showPlay:(state) => state.showPlay,
+    showAbout:(state) => state.showAbout,
+    showLogin:(state) => state.showLogin,
+    day:(state) => state.day,
   },
   mutations: {
     SET_TOKEN: (state, token) => {
@@ -31,8 +54,84 @@ export default new Vuex.Store({
     SET_VEHICLEINFO: (state, vehicleInfo) => {
       state.vehicleInfo = vehicleInfo
     },
+    setMusicList: (state, playload) => {
+        state.musicList = playload
+        storage.saveMusic(playload)
+    },
+    play: state => {
+        state.isPlaying = true
+
+    },
+    pause: state => {
+        state.isPlaying = false
+    },
+    clear: state => {
+        state.musicList = {'musicData':[]}
+        storage.saveMusic({'musicData':[]})
+    },
+    setPlayIndex: (state, playload) => {
+        state.nowPlayIndex = playload
+    },
+    setShowMiniAudio: (state, playload) => {
+        state.showMiniAudio = playload
+    },
+    setCommendList: (state, playload) => {
+        state.commendList = playload
+    },
+    setCurTimeNum: (state, playload) => {
+        state.curTimeNum = playload
+    },
+    setShowPlay: (state, playload) => {
+        state.showPlay = playload
+    },
+    modifyMusicList: (state, playload) => {
+        for (let i = 0, length = state.musicList.musicData.length; i < length; i++) {
+            if (state.musicList.musicData[i].id == playload.id) {
+                return
+            }
+        }
+        state.musicList.musicData.splice(state.nowPlayIndex + 1, 0, playload)
+        state.nowPlayIndex = state.nowPlayIndex + 1
+        storage.saveMusic(playload)
+    },
+    setShowAbout: (state, playload) => {
+        state.showAbout = playload
+    },
+    setShowLogin: (state, playload) => {
+        state.showLogin = playload
+    },
+    setDay: (state, playload) => {
+        state.day = playload
+    },
+    setTheme: (state, playload) => {
+        state.theme = playload
+    }
   },
   actions: {
+    getInitData() {
+        return new Promise((resolve, reject) => {
+            Vue.axios.get('http://oyhfe.com:666/music-data')
+                .then(res => {
+                    resolve(res.data.musicData)
+                })
+        })
+    },
+    getInitCommend() {
+        return new Promise((resolve, reject) => {
+            Vue.axios.get('http://oyhfe.com:666/commend-list')
+                .then(res => {
+                    resolve(res.data.commendList)
+                })
+        })
+    },
+    getInitDay() {
+        return new Promise((resolve, reject) => {
+            Vue.axios.get('http://oyhfe.com:666/day')
+                .then(res => {
+                    resolve(res.data.day)
+                })
+        })
+    },
     Login({ commit }, userInfo) {
       return new Promise((resolve, reject) => {
         fetch({
